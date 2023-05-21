e10335389ead0ca160da6d1d71c2f177a0e7faeb
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-21 16:21:21 +0900

    Drawer 아이템 구성 및 타일윈드 중앙 정렬

--- app/page.tsx
+++ app/page.tsx
@@ -24,18 +24,15 @@ export default function Home() {
       <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
         <Card
           icon={
-            <button
-              className={`bg-green-600 text-white rounded px-4 py-1`}
-            >hello</button>
+            // Tailwind center -> flex flex-wrap justify-center items-center
+            <button className={`h-12 w-12 flex flex-wrap justify-center items-center bg-green-600 text-white rounded px-4 py-1`}>
+              Dream
+            </button>
           }
+          title={<p>상상코칭</p>}
+          descrision={<p className="text-sm text-gray-900 dark:text-white">아이의 미래를 위한 선택 상상코칭입니다.</p>}
+          subDescrision={<p className="text-xs text-gray-900 dark:text-white">학생 맞춤의 프리미엄 과외</p>}
         />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
       </Drawer>
     </div>
   );
--- components/drawer-card.tsx
+++ components/drawer-card.tsx
@@ -1,15 +1,30 @@
 import { ReactNode } from "react";
 
-export default function Card({ icon }: { icon?: ReactNode }) {
+export default function Card({
+  icon,
+  title,
+  descrision,
+  subDescrision,
+}: {
+  icon?: ReactNode;
+  title?: ReactNode;
+  descrision?: ReactNode;
+  subDescrision?: ReactNode;
+}) {
   return (
     <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
       <div className="animate-pulse flex space-x-4">
-        <div className="rounded-full bg-gray-400 h-12 w-12 overflow-auto">{icon}</div>
+        <div className="rounded-full bg-gray-400 h-12 w-12 overflow-auto">
+          {icon}
+        </div>
         <div className="flex-1 space-y-4 py-1">
-          <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+          {/* <div className="h-4 bg-gray-400 rounded w-3/4">{title}</div> */}
+          <div className="h-4 w-3/4">{title}</div>
           <div className="space-y-2">
-            <div className="h-4 bg-gray-400 rounded"></div>
-            <div className="h-4 bg-gray-400 rounded w-5/6"></div>
+            {/* <div className="h-4 bg-gray-400 rounded">{script1}</div> */}
+            <div className="h-4 rounded">{descrision}</div>
+            {/* <div className="h-4 bg-gray-400 rounded w-5/6">{script2}</div> */}
+            <div className="h-4 rounded w-5/6">{subDescrision}</div>
           </div>
         </div>
       </div>
