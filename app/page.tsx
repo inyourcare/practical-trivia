a06b364c200633167ef4f368c8bfea760b558b84
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-21 20:46:22 +0900

    상상코칭 페이지

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,7 @@ export default function Home() {
           title={<p>상상코칭</p>}
           descrision={<p className="text-sm text-gray-900 dark:text-white">아이의 미래를 위한 선택 상상코칭입니다.</p>}
           subDescrision={<p className="text-xs text-gray-900 dark:text-white">학생 맞춤의 프리미엄 과외</p>}
+          targetLink="/sangsang/"
         />
       </Drawer>
     </div>
--- components/drawer-card.tsx
+++ components/drawer-card.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function Card({
@@ -5,21 +6,24 @@ export default function Card({
   title,
   descrision,
   subDescrision,
+  targetLink = '#',
 }: {
   icon?: ReactNode;
   title?: ReactNode;
   descrision?: ReactNode;
   subDescrision?: ReactNode;
+  targetLink?: string;
 }) {
   return (
     <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-      <div className="animate-pulse flex space-x-4">
+      {/* <div className="animate-pulse flex space-x-4"> */}
+      <div className="flex space-x-4">
         <div className="rounded-full bg-gray-400 h-12 w-12 overflow-auto">
           {icon}
         </div>
         <div className="flex-1 space-y-4 py-1">
           {/* <div className="h-4 bg-gray-400 rounded w-3/4">{title}</div> */}
-          <div className="h-4 w-3/4">{title}</div>
+          <div className="h-4 w-3/4"><Link href={targetLink}>{title}</Link></div>
           <div className="space-y-2">
             {/* <div className="h-4 bg-gray-400 rounded">{script1}</div> */}
             <div className="h-4 rounded">{descrision}</div>
