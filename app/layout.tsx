fe6c317c90eb75cd192d2588ab2fbcdeb2ff4b6d
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-22 00:15:36 +0900

    레이아웃 활용을 좀 더 다각화

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "실용주의 잡학사전의 공부 컨설팅",
-  description: "과외 선생님 구하기 부터 입시 컨설팅까지, 초등,중등,고등 바른교육을 위한 컨설팅을 제공합니다.",
-  keywords: "과외선생님, 온라인과외, 화상과외, 대학생과외, 과외구하기, 선생님찾기, 과외찾기",
+  description:
+    "과외 선생님 구하기 부터 입시 컨설팅까지, 초등,중등,고등 바른교육을 위한 컨설팅을 제공합니다.",
+  keywords:
+    "과외선생님, 온라인과외, 화상과외, 대학생과외, 과외구하기, 선생님찾기, 과외찾기",
 };
 
 export default function RootLayout({
@@ -19,31 +21,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <Drawer>
-        <DrawerCard
-          icon={
-            // Tailwind center -> flex flex-wrap justify-center items-center
-            <button
-              className={`h-12 w-12 flex flex-wrap justify-center items-center bg-green-600 text-white rounded px-4 py-1`}
-            >
-              Dream
-            </button>
-          }
-          title={<p>상상코칭</p>}
-          descrision={
-            <p className="text-sm text-gray-900 dark:text-white">
-              아이의 미래를 위한 선택 상상코칭입니다.
-            </p>
-          }
-          subDescrision={
-            <p className="text-xs text-gray-900 dark:text-white">
-              학생 맞춤의 프리미엄 과외
-            </p>
-          }
-          targetLink="/sangsang/"
-        />
-      </Drawer>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Drawer>
+          <DrawerCard
+            icon={
+              // Tailwind center -> flex flex-wrap justify-center items-center
+              <button
+                className={`h-12 w-12 flex flex-wrap justify-center items-center bg-green-600 text-white rounded px-4 py-1`}
+              >
+                Dream
+              </button>
+            }
+            title={<p>상상코칭</p>}
+            descrision={
+              <p className="text-sm text-gray-900 dark:text-white">
+                아이의 미래를 위한 선택 상상코칭입니다.
+              </p>
+            }
+            subDescrision={
+              <p className="text-xs text-gray-900 dark:text-white">
+                학생 맞춤의 프리미엄 과외
+              </p>
+            }
+            targetLink="/sangsang/"
+          />
+        </Drawer>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/sangsang/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "실용주의 잡학사전의 공부 컨설팅::상상코칭",
+  description:
+    "상상코칭 통해 나에게 알맞은 과외 선생님을 찾아보세요",
+  keywords:
+    "과외선생님, 온라인과외, 화상과외, 대학생과외, 과외구하기, 선생님찾기, 과외찾기",
+};
+
+
+export default function SangsangLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
+}
