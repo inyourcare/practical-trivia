f2c1d08ae6ff294ef25fd70bf4788e1d3764f8cc
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-21 22:53:12 +0900

    상상코칭 이니셜 페이지 추가

--- app/sangsang/page.tsx
+++ app/sangsang/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function SangSangHome() {
@@ -5,11 +7,11 @@ export default function SangSangHome() {
   // const { menu, submenu, detail } = router.query
   return (
     <div>
-      <Link href={'/'}>
+      <Link href={"/"}>
         <div
           className={`absolute top-0 left-0 bg-gray-300 text-white rounded px-4 py-1 bg-opacity-25 hover:bg-opacity-100`}
           style={{
-            backgroundColor: 'green',
+            backgroundColor: "green",
             backgroundImage: `url('/images/icons/list.svg')`,
             backgroundRepeat: `no-repeat`,
             backgroundPosition: `center`,
@@ -18,6 +20,33 @@ export default function SangSangHome() {
           }}
         ></div>
       </Link>
+      {[[1,1863/1920], [2,1392/1920], [3,1392/1920], [4,1144/1920], [5,1863/2139]].map((order) => {
+        return (
+          <div
+            key={order.at(0)}
+            style={{
+              backgroundImage:
+                `url('/images/business/sangsang/` + order.at(0) + `.png')`,
+              backgroundRepeat: `no-repeat`,
+              backgroundPosition: `center`,
+              backgroundSize: "100% 100%",
+              maxWidth: '1920px',
+              width: "100%",
+              height: "0px",
+              paddingTop: `${Math.round(Number(order.at(1)))*100}%`,
+              // paddingTop: `100%`,
+            }}
+          >
+            {/* <Image
+              key={order}
+              src={`/images/business/sangsang/` + order + `.png`}
+              fill
+              style={{ visibility: "hidden" }}
+              alt=""
+            /> */}
+          </div>
+        );
+      })}
     </div>
   );
 }
