7680ba22e6191d6c5f7f339f803a513df26f8d5f
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-21 16:52:24 +0900

    배경 opacity 처리 추가

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -12,21 +12,24 @@ export default function Drawer({
   return (
     <main
       className={
-        "fixed inset-y-0 right-0 overflow-hidden z-10 bg-gray-900 bg-opacity-25 transform ease-in-out " +
+        "fixed inset-y-0 right-0 overflow-hidden transform ease-in-out bg-gray-600 bg-opacity-0 " +
         (isOpen
-          // ? " transition-opacity opacity-100 duration-500 translate-x-0  "
-          ? " transition-opacity opacity-100 duration-500 "
-          // : " transition-all delay-500 opacity-0 translate-x-full  ")
-          : " -translate-x-full transition-all opacity-0 delay-500")
+          ? "transition-all z-10 duration-500 bg-opacity-40"
+          : "transition-all duration-500 -z-10 ")
+        // + (isOpen
+        //   // ? " transition-opacity opacity-100 duration-500 translate-x-0  "
+        //   ? " translate-x-0 transition-opacity duration-500 "
+        //   // : " transition-all delay-500 opacity-0 translate-x-full  ")
+        //   : " -translate-x-full transition-all delay-500")
       }
     >
-    {/* <main id="drawer-example" className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" aria-labelledby="drawer-label"> */}
+      {/* <main id="drawer-example" className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" aria-labelledby="drawer-label"> */}
       <section
         className={
           // " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
           " w-screen max-w-lg right-full absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
           // (isOpen ? " translate-x-0 " : " translate-x-full ")
-          (isOpen ? " translate-x-full " : " translate-x-0 ")
+          (isOpen ? " translate-x-full opacity:100" : " translate-x-0 ")
         }
       >
         <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
