b7b7a529e174f6739282ba89d2e994517b0e347f
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-23 22:22:56 +0900

    다이얼로그 닫기버튼 작업

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -17,22 +17,34 @@ export default function Drawer({
   // }, [pathname]);
   return (
     <>
-      {/* <div className={`absolute h-screen w-full bg-green-300 opacity-0`}> */}
-      <div
-        className={`sticky top-[90%] h-0 opacity-25 hover:opacity-100 flex flex-wrap justify-center items-center`}
+      <main
+        //  className={`sticky top-[30%] h-0 flex flex-wrap justify-center items-center w-full`}
+        className={`sticky top-[75%] h-0 flex flex-col justify-center items-center w-full`}
       >
+        <section
+          className={
+            "w-3/5 min-h-[300px] bg-gray-100 rounded-xl border border-black " +
+            (isOpen ? "visible":"invisible")
+          }
+        >
+          <header className="flex flex-row-reverse">
+            <button
+              className={`w-5 h-5 m-5 bg-[url('/images/icons/x.svg')] bg-no-repeat bg-center bg-cover cursor-pointer `}
+              onClick={() => setIsOpen(false)}
+            ></button>
+          </header>
+        </section>
         <button
           className={
-            `bg-sul-btn rounded-xl
-            xl:w-[700px] xl:h-[60px] w-[700px] h-[60px]`
+            `bg-sul-btn rounded-xl opacity-25 hover:opacity-100
+            w-3/5 min-h-[60px] my-10`
             // bg-[url('/images/icons/list.svg')] bg-no-repeat bg-center`
           }
-          onClick={() => setIsOpen(true)}
+          onClick={() => setIsOpen(!isOpen)}
         >
           <span className={`text-white font-semibold`}>{btnTitle}</span>
         </button>
-      </div>
-      {/* </div> */}
+      </main>
     </>
   );
 }
