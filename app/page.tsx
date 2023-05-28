694fd50c594aa635dddc728b5dafc6fd7883dd95
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-28 16:36:37 +0900

    carousel 안쓰니까

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // import styles from "./page.module.css";
-import CarouselCard from "@/components/carousel/CarouselCard";
-import CarouselIframes from "@/components/carousel/carousel-iframes";
+import CarouselCard from "@/components/carousel-deprecated/CarouselCard";
+import CarouselIframes from "@/components/carousel-deprecated/carousel-iframes";
 import Card from "@/components/drawer/drawer-card";
 
 export default function Home() {
@@ -10,27 +10,6 @@ export default function Home() {
         <div className="flex justify-center items-center flex-col w-[80%] h-[80%] bg-black">
           <div className="w-full h-[50%] bg-red-300"></div>
           <div className="flex justify-center items-center w-full h-[50%] bg-green-300 ">
-            {/* <iframe
-              width="560"
-              height="315"
-              // src="https://www.youtube.com/embed/pLt0AFEppQ8"
-              src="https://youtube.com/embed/REhoCORAl3A"
-              title="YouTube video player"
-              // frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              // allowfullscreen
-              allowFullScreen
-            ></iframe> */}
-            {/* "https://www.youtube.com/embed/3vzq2_NzF_s" "https://www.youtube.com/embed/eyD4mLiP-Ow" "https://youtube.com/embed/REhoCORAl3A" */}
-            <CarouselIframes
-              items={[
-                // "https://www.youtube.com/embed/pLt0AFEppQ8",
-                "https://www.youtube-nocookie.com/embed/pLt0AFEppQ8",
-                "https://www.youtube-nocookie.com/embed/3vzq2_NzF_s",
-                "https://www.youtube-nocookie.com/embed/eyD4mLiP-Ow",
-                "https://www.youtube-nocookie.com/embed/REhoCORAl3A",
-              ]}
-            ></CarouselIframes>
           </div>
         </div>
       </div>
