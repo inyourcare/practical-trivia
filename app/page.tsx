11a6b74c6d9fde227b12758d1fddd2f3c948796a
Kwon KiHoon <inyourcareallen@gmail.com>
2023-05-28 17:54:14 +0900

    posts 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,38 @@
 // import styles from "./page.module.css";
 import Header from "@/components/Header";
-import CarouselCard from "@/components/carousel-deprecated/CarouselCard";
-import CarouselIframes from "@/components/carousel-deprecated/carousel-iframes";
 import Card from "@/components/drawer/drawer-card";
+import dayjs from "dayjs";
+// import { posts as data } from "../data/post";
+import { posts } from "../data/post";
+import Post from "@/components/Post";
+import Pagination from "@/components/Pagination";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <div className={`flex justify-center items-center h-[100vh]`}>
-        <div className="flex justify-center items-center flex-col w-[80%] h-[80%] bg-black">
-          <div className="w-full h-[50%] bg-red-300"></div>
-          <div className="flex justify-center items-center w-full h-[50%] bg-green-300 ">
-          </div>
-        </div>
-      </div>
+      <Header />
+      <main className="container mx-auto flex flex-col p-3">
+        <p className="text-4xl inline font-bold text-left ml-0  sm:ml-0 md:ml-10 lg:ml-10 xl:ml-10 2xl:ml-24  my-16 leading-[normal] text-[rgba(35,46,82,1)]">
+          All posts
+        </p>
+
+        {posts?.map((item) => {
+          let GetDate = dayjs(item.date).format("DD-MMM , YYYY");
+
+          return (
+            <Post
+              key={item.id}
+              tag={item.tags[0]}
+              date={GetDate.toString()}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+            />
+          );
+        })}
+
+        <Pagination />
+      </main>
     </div>
   );
 }
