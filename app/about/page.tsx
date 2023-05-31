3a11c3fd0865b7a2f1a3a2581ae30d4c1230616f
inyourcare <inyourcareallen@gmail.com>
2023-05-31 12:20:10 +0900

    about page 작업중 저장

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,8 @@ export default function SangSangHome() {
       <header className="w-full flex flex-col py-5 bg-[rgba(35,46,82,1)]">
         <Nav />
       </header>
-      <div className='py-12'
+      <div
+        className="py-12"
         style={{
           backgroundImage: `url('` + post.data.image + `')`,
           backgroundRepeat: `no-repeat`,
@@ -26,22 +27,13 @@ export default function SangSangHome() {
           // paddingTop: `100%`,
         }}
       >
-        {/* <Header /> */}
-        {/* <PostHeader
-        title={post.data.title}
-        tag={post.data.krTags[0]}
-        date={dayjs(post.data.date).format("DD MMMM , YYYY")}
-        authorName={post.data.author}
-      /> */}
-        {/* <div className="my-10 mx-auto">
-        <Image
-          height="250"
-          width="500"
-          src={post.data.image}
-          alt={post.data.imageAlt}
-          className="mx-auto h-[72%] w-[1424px]"
-        />
-      </div> */}
+        <div className="w-full flex p-3">
+          <div className="flex-1 p-3 flex justify-center items-center">
+            <Image width={300} height={300} src={'/images/camera.jpg'} alt="Image Description" />
+          </div>
+          <div className="h-20 bg-gray-200 flex-1 p-3">section2</div>
+          <div className="h-20 bg-gray-200 flex-1 p-3">section3</div>
+        </div>
 
         <div className="my-12 prose prose-stone lg:prose-lg mx-auto">
           <Markdown>{post.content}</Markdown>
