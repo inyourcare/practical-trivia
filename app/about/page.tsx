9611125c02b36e61fdb90e483e7ed611ecb65301
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-01 21:32:28 +0900

    about avatar 추가 및 css 몇가지 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,7 @@ export default function SangSangHome() {
         <Nav />
       </header>
       <div
-        className="py-12"
+        className="py-12 flex justify-center"
         style={{
           backgroundImage: `url('` + post.data.image + `')`,
           backgroundRepeat: `no-repeat`,
@@ -29,37 +29,61 @@ export default function SangSangHome() {
           // paddingTop: `100%`,
         }}
       >
-        <div className="w-full flex p-3 flex-col md:flex-row">
-          <div className="flex-1 p-3 flex justify-center items-start">
-            <Image
-              width={300}
-              height={300}
-              src={"/images/camera.jpg"}
-              alt="Image Description"
-            />
+        <div className="w-full max-w-screen-lg">
+          <div className="w-full flex p-3 flex-col md:flex-row">
+            <div className="flex-1 p-3 flex justify-center items-start">
+              <Image
+                width={300}
+                height={300}
+                src={"/images/camera.jpg"}
+                alt="Image Description"
+              />
+            </div>
+            <div className="flex-1 p-3">
+              {/* <h1 className="mb-2">{`The Short`}</h1> */}
+              <h1 className={`${styles.titleText} mb-4`}>{`짧게`}</h1>
+              <p
+                className={`${styles.text} mb-2`}
+              >{`실용주의 잡학사전은 사람과 관계, 근원적 의문, 교육에 관심이 있습니다.`}</p>
+              <p
+                className={`${styles.text}`}
+              >{`자라나는 학생들이 스스로 만족하며 살아갈 수 있도록 도움을 주고 싶습니다. 교육이 이러한 해답을 제공할 수 있다고 믿습니다.`}</p>
+            </div>
+            <div className="flex-1 p-3">
+              <h1 className={`${styles.titleText} mb-4`}>{`길게`}</h1>
+              <p
+                className={`${styles.subText} mb-2`}
+              >{`실용주의 잡학사전은 대한민국의 대구라는 도시에서 태어났습니다. 그는 삶에 모순이 많다고 생각했습니다. 의문은 늘어났지만 해답을 찾을수는 없었습니다.`}</p>
+              <p
+                className={`${styles.subText} mb-2`}
+              >{`허무함에 빠져 핑계로 가득찬 하루하루를 보내던 그는 어느날, 차분히 한걸음씩 걸어 가 보면 어떨까라는 생각을 합니다. 삶에대한 태도, 가치관, 버릇, 습관. 보이는 하나하나 차근차근 개선 해 나갑니다.`}</p>
+              <p
+                className={`${styles.subText} mb-2`}
+              >{`그리고 그런 그에게 이제는 꿈이 생겼습니다. 그가 겪은 어려움을 그 다음 세대는 겪지 않았으면 좋겠다고 생각합니다. 살기 위해 일하는 것이 아니라 목표를 이루기 위해 살아갑니다. 내면의 출구를 향한 걸음을 끝내고, 세상을 향한 걸음을 나아가 보려합니다. 그 첫걸음.`}</p>
+            </div>
           </div>
-          <div className="flex-1 p-3">
-            {/* <h1 className="mb-2">{`The Short`}</h1> */}
-            <h1 className={`${styles.titleText} mb-4`}>{`짧게`}</h1>
-            <p className={`${styles.text} mb-2`}>{`실용주의 잡학사전은 사람과 관계, 근원적 의문, 교육에 관심이 있습니다.`}</p>
-            <p className={`${styles.text}`}>{`자라나는 학생들이 스스로 만족하며 살아갈 수 있도록 도움을 주고 싶습니다. 교육이 이러한 해답을 제공할 수 있다고 믿습니다.`}</p>
-          </div>
-          <div className="flex-1 p-3">
-            <h1 className={`${styles.titleText} mb-4`}>{`길게`}</h1>
-            <p className={`${styles.subText} mb-2`}>{`실용주의 잡학사전은 대한민국의 대구라는 도시에서 태어났습니다. 그는 삶에 모순이 많다고 생각했습니다. 의문은 늘어났지만 해답을 찾을수는 없었습니다.`}</p>
-            <p className={`${styles.subText} mb-2`}>{`허무함에 빠져 핑계로 가득찬 하루하루를 보내던 그는 어느날, 차분히 한걸음씩 걸어 가 보면 어떨까라는 생각을 합니다. 삶에대한 태도, 가치관, 버릇, 습관. 보이는 하나하나 차근차근 개선 해 나갑니다.`}</p>
-            <p className={`${styles.subText} mb-2`}>{`그리고 그런 그에게 이제는 꿈이 생겼습니다. 그가 겪은 어려움을 그 다음 세대는 겪지 않았으면 좋겠다고 생각합니다. 살기 위해 일하는 것이 아니라 목표를 이루기 위해 살아갑니다. 내면의 출구를 향한 걸음을 끝내고, 세상을 향한 걸음을 나아가 보려합니다. 그 첫걸음.`}</p>
-          </div>
-        </div>
 
-        <div className="my-12 prose prose-stone lg:prose-lg mx-auto">
-          <Markdown>{post.content}</Markdown>
-          소개 페이지 목록
-          <ul>
-            <li><Link href={`${process.env.HOST_BASE_URL}/intro/howcoding`}>하우코딩</Link></li>
-            <li><Link href={`${process.env.HOST_BASE_URL}/intro/goodo`}>공부구도</Link></li>
-            <li><Link href={`${process.env.HOST_BASE_URL}/intro/sangsang`}>상상코칭</Link></li>
-          </ul>
+          <div className="my-12 prose prose-stone lg:prose-lg mx-auto">
+            <Markdown>{post.content}</Markdown>
+            소개 페이지 목록
+            <ul>
+              <li>
+                <Link href={`${process.env.HOST_BASE_URL}/intro/howcoding`}>
+                  하우코딩
+                </Link>
+              </li>
+              <li>
+                <Link href={`${process.env.HOST_BASE_URL}/intro/goodo`}>
+                  공부구도
+                </Link>
+              </li>
+              <li>
+                <Link href={`${process.env.HOST_BASE_URL}/intro/sangsang`}>
+                  상상코칭
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
--- data/about.md
+++ data/about.md
@@ -11,24 +11,19 @@ author: "실용주의 잡학사전"
 krTags: ["About", "실용주의 잡학사전"]
 ---
 
+
 <p className="mb-3 font-light text-gray-800 dark:text-gray-800 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
 </p>
+<!-- <div className="py-3.5 text-left font-normal rounded-bl-[10px] rounded-br-[10px] bg-[rgba(235,242,254,1)] text-[rgba(35,46,82,1)]"> -->
+<div className="py-3.5 text-left font-normal rounded-bl-[10px] rounded-br-[10px] bg-transparent text-[rgba(35,46,82,1)] flex justify-center">
+  <img height="300px" width="300px" className="cover" alt="" src="/images/avatar.png"/>
+  <p className="text-xs leading-normal text-center uppercase">
+  </p>
+</div>
 <p className="mb-3 font-light first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
 안녕하세요 실용주의 잡학사전입니다. 이 웹사이트는 제가 직접 만들었습니다. 지금은 마케터로서 각 교육 플랫폼의 소개와 상담 역할을 하고 있습니다.
 이 사이트를 시작으로 저만의 교육 플랫폼을 만드는 것이 목표입니다. 개발 관련 문의를 주셔도 좋습니다. 항상 좋은일만 가득하길 바랍니다. :D 
 </p>
-<div className="py-3.5 text-left font-normal rounded-bl-[10px] rounded-br-[10px] bg-[rgba(235,242,254,1)] text-[rgba(35,46,82,1)]">
-  <!-- <img
-      height="324px"
-      width="1424px"
-      className="cover"
-      alt="Alt for images"
-      src="https://source.unsplash.com/random/100x100"
-  />
-  <p className="text-xs leading-normal text-center uppercase">
-      Image caption or credit
-  </p> -->
-</div>
 <p className="mb-3 font-light text-gray-500 dark:text-gray-400">
 </p>
 <p className="mb-3 font-light text-gray-500 dark:text-gray-400">
