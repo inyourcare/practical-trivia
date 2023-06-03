045a04e77f29c934ad89e583e62f4a09d158fc07
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-03 21:01:23 +0900

    페이지네이션 앞뒤 버튼 div 크기 조정

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -31,7 +31,8 @@ function Pagination({postPreviews}:{postPreviews:JSX.Element[]}) {
           Entries
         </span>
 
-        <div className="inline-flex justify-evenly w-5/6 sm:w-5/6 md:w-3/6 lg:w-3/6 xl:w-4/6 2xl:w-2/6">
+        {/* <div className="inline-flex justify-evenly w-5/6 sm:w-5/6 md:w-3/6 lg:w-3/6 xl:w-4/6 2xl:w-2/6"> */}
+        <div className="inline-flex justify-evenly w-7/12 sm:w-7/12 md:w-5/12 lg:w-5/12 xl:w-6/12 2xl:w-4/12">
           <button
             // href={"/"}
             className="h-10 mx-auto  w-[100px] my-14 gap-2.5 inline-flex flex-col justify-center items-center rounded-lg text-center font-medium bg-[rgba(235,242,254,1)] text-[rgba(35,46,82,1)]"
