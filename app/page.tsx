28d66613a4f0c7185cfc2cf807ab55e787571d7d
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-03 21:20:40 +0900

    post 순서 수정

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ import getPostMetadata from "@/components/post/getPostMetadata";
 import PostPreview from "@/components/post/PostPreview";
 
 export default function Home() {
-  const postMetadata = getPostMetadata();
+  const postMetadata = getPostMetadata().sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
   const postPreviews = postMetadata.map((post) => (
     <PostPreview key={post.slug} {...post} />
   ));
