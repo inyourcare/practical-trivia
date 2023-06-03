7f54bd8005bcd9dc4f48e2857a5b9a884ec08e1c
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-04 05:57:48 +0900

    공부구도,하우코딩 intro 페이지 틀변경

--- app/intro/goodo/page.tsx
+++ app/intro/goodo/page.tsx
@@ -1,49 +1,342 @@
-"use client";
-
+import Image from "next/image";
 export default function GoodoHome() {
-  // const router = useRouter()
-  // const { menu, submenu, detail } = router.query
   return (
-    <div className="w-full flex flex-col justify-center items-center">
-      {[
-        [1, 720 / 1100],
-        [2, 1507 / 1100],
-        [3, 529 / 1100],
-        [4, 712 / 1079],
-        [5, 2022 / 1100],
-        [6, 1096 / 1100],
-        [7, 817 / 1100],
-        [8, 603 / 1048],
-        [9, 621 / 1100],
-        [10, 873 / 1100],
-        [11, 756 / 1100],
-      ].map((order) => {
-        return (
-          <div
-            key={order.at(0)}
-            style={{
-              backgroundImage:
-                `url('/images/intro/goodo/` + order.at(0) + `.png')`,
-              backgroundRepeat: `no-repeat`,
-              backgroundPosition: `center`,
-              backgroundSize: "100% 100%",
-              maxWidth: "1080px",
-              width: "100%",
-              height: "0px",
-              paddingTop: `${Math.round(Number(order.at(1))) * 100}%`,
-              // paddingTop: `100%`,
-            }}
-          >
-            {/* <Image
-              key={order}
-              src={`/images/business/sangsang/` + order + `.png`}
-              fill
-              style={{ visibility: "hidden" }}
+    <>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center w-full bg-gray-300">
+          <Image
+            width={1200}
+            height={700}
+            src={`/images/intro/sangsang/1.webp`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="h-[400px] flex justify-center items-center">
+        <Image
+          width={1040}
+          height={161}
+          src={`/images/intro/sangsang/2.png`}
+          alt=""
+        />
+      </div>
+
+      <div className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={650}
+            height={108}
+            src={`/images/intro/sangsang/3.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-nowrap">
+          <Image
+            width={450 / 2}
+            height={490 / 2}
+            src={`/images/intro/sangsang/review01.png`}
+            alt=""
+          />
+          <Image
+            width={450 / 2}
+            height={490 / 2}
+            src={`/images/intro/sangsang/review02.png`}
+            alt=""
+          />
+          <Image
+            width={450 / 2}
+            height={490 / 2}
+            src={`/images/intro/sangsang/review03.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-nowrap">
+          <Image
+            width={450 / 2}
+            height={490 / 2}
+            src={`/images/intro/sangsang/review04.png`}
+            alt=""
+          />
+          <Image
+            width={450 / 2}
+            height={490 / 2}
+            src={`/images/intro/sangsang/review05.png`}
+            alt=""
+          />
+          <Image
+            width={450 / 2}
+            height={490 / 2}
+            src={`/images/intro/sangsang/review06.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center flex-col">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={477}
+            height={103}
+            src={`/images/intro/sangsang/4.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center flex-row ">
+          <div className="h-[400px] flex justify-center items-center flex-1">
+            <Image
+              width={414}
+              height={174}
+              src={`/images/intro/sangsang/5.png`}
               alt=""
-            /> */}
+            />
           </div>
-        );
-      })}
-    </div>
+          <div className="flex justify-center items-center flex-1 flex-col">
+            <Image
+              width={630}
+              height={501}
+              src={`/images/intro/sangsang/5-1.png`}
+              alt=""
+            />
+            <Image
+              width={630}
+              height={43}
+              src={`/images/intro/sangsang/5-2.png`}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={559}
+            height={225}
+            src={`/images/intro/sangsang/6.png`}
+            alt=""
+          />
+        </div>
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={1200}
+            height={347}
+            src={`/images/intro/sangsang/6-1.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col items-center p-20 pb-40">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={490}
+            height={103}
+            src={`/images/intro/sangsang/7.png`}
+            alt=""
+          />
+        </div>
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={1200}
+            height={354}
+            src={`/images/intro/sangsang/7-1.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={608}
+            height={104}
+            src={`/images/intro/sangsang/8.png`}
+            alt=""
+          />
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <div className="h-[400px] flex justify-center items-center flex-1">
+            <Image
+              width={636}
+              height={271}
+              src={`/images/intro/sangsang/8-1.png`}
+              alt=""
+            />
+          </div>
+          <div className="h-[400px] flex justify-center items-center flex-1">
+            <Image
+              width={600}
+              height={400}
+              src={`/images/intro/sangsang/8-4.png`}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <div className="flex justify-center items-center basis-1/2">
+            <Image
+              width={600}
+              height={385}
+              src={`/images/intro/sangsang/8-2.png`}
+              alt=""
+            />
+          </div>
+          <div className="flex justify-center items-center basis-1/2">
+            <Image
+              width={314}
+              height={359}
+              src={`/images/intro/sangsang/8-3.png`}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col items-center p-20 pb-40">
+        <div className="h-[200px] flex justify-center items-center">
+          <Image
+            width={481}
+            height={107}
+            src={`/images/intro/sangsang/9.png`}
+            alt=""
+          />
+        </div>
+        <div className="h-[200px] flex justify-center items-center">
+          <Image
+            width={326}
+            height={93}
+            src={`/images/intro/sangsang/9-1.png`}
+            alt=""
+          />
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <div className="flex justify-center items-center basis-1/4">
+            <Image
+              width={260}
+              height={300}
+              src={`/images/intro/sangsang/9-2.jpg`}
+              alt=""
+            />
+          </div>
+          <div className="flex justify-center items-center basis-1/4">
+            <Image
+              width={260}
+              height={300}
+              src={`/images/intro/sangsang/9-3.jpg`}
+              alt=""
+            />
+          </div>
+          <div className="flex justify-center items-center basis-1/4">
+            <Image
+              width={260}
+              height={300}
+              src={`/images/intro/sangsang/9-4.jpg`}
+              alt=""
+            />
+          </div>
+          <div className="flex justify-center items-center basis-1/4">
+            <Image
+              width={260}
+              height={300}
+              src={`/images/intro/sangsang/9-5.jpg`}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="h-[200px] flex justify-center items-center">
+          <Image
+            width={1200}
+            height={130}
+            src={`/images/intro/sangsang/9-6.jpg`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div
+        className="flex justify-center flex-col items-center p-20 pb-40"
+        style={{
+          backgroundImage: `url('/images/intro/sangsang/10-back.jpg')`,
+          backgroundRepeat: `no-repeat`,
+          backgroundPosition: `center`,
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={275}
+            height={92}
+            src={`/images/intro/sangsang/10.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-nowrap">
+          <Image
+            width={261}
+            height={415}
+            src={`/images/intro/sangsang/10-1.png`}
+            alt=""
+            className="p-1"
+          />
+          <Image
+            width={261}
+            height={415}
+            src={`/images/intro/sangsang/10-2.png`}
+            alt=""
+            className="p-1"
+          />
+          <Image
+            width={261}
+            height={415}
+            src={`/images/intro/sangsang/10-3.png`}
+            alt=""
+            className="p-1"
+          />
+        </div>
+        <div className="flex flex-nowrap">
+          <Image
+            width={261}
+            height={415}
+            src={`/images/intro/sangsang/10-4.png`}
+            alt=""
+            className="p-1"
+          />
+          <Image
+            width={261}
+            height={415}
+            src={`/images/intro/sangsang/10-5.png`}
+            alt=""
+            className="p-1"
+          />
+          <Image
+            width={261}
+            height={415}
+            src={`/images/intro/sangsang/10-6.png`}
+            alt=""
+            className="p-1"
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div
+          className="flex justify-center items-center w-full"
+          style={{
+            backgroundImage: `url('/images/intro/sangsang/12-back.webp')`,
+            backgroundRepeat: `no-repeat`,
+            backgroundPosition: `center`,
+            backgroundSize: "100% 100%",
+          }}
+        >
+          <Image
+            width={1200}
+            height={1218}
+            src={`/images/intro/sangsang/12.webp`}
+            alt=""
+          />
+        </div>
+      </div>
+    </>
   );
 }
