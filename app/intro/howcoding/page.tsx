7c841ec74d3e63884da727c01ce31e9a60b589b5
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-04 07:21:12 +0900

    하우코딩 intro 페이지 완성

--- app/intro/howcoding/page.tsx
+++ app/intro/howcoding/page.tsx
@@ -3,11 +3,19 @@ export default function HowcodingHome() {
   return (
     <>
       <div className="flex justify-center items-center">
-        <div className="flex justify-center items-center w-full bg-gray-300">
+        <div
+          className="w-full flex justify-center items-center w-full bg-gray-300"
+          style={{
+            backgroundImage: `url('/images/intro/howcoding/1-back.png')`,
+            backgroundRepeat: `no-repeat`,
+            backgroundPosition: `center`,
+            backgroundSize: "100% 100%",
+          }}
+        >
           <Image
             width={1200}
-            height={700}
-            src={`/images/intro/sangsang/1.webp`}
+            height={1515}
+            src={`/images/intro/howcoding/1.png`}
             alt=""
           />
         </div>
@@ -17,247 +25,24 @@ export default function HowcodingHome() {
         <Image
           width={1040}
           height={161}
-          src={`/images/intro/sangsang/2.png`}
+          src={`/images/intro/howcoding/2.png`}
           alt=""
         />
       </div>
 
-      <div className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40">
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={650}
-            height={108}
-            src={`/images/intro/sangsang/3.png`}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-nowrap">
-          <Image
-            width={450 / 2}
-            height={490 / 2}
-            src={`/images/intro/sangsang/review01.png`}
-            alt=""
-          />
-          <Image
-            width={450 / 2}
-            height={490 / 2}
-            src={`/images/intro/sangsang/review02.png`}
-            alt=""
-          />
-          <Image
-            width={450 / 2}
-            height={490 / 2}
-            src={`/images/intro/sangsang/review03.png`}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-nowrap">
-          <Image
-            width={450 / 2}
-            height={490 / 2}
-            src={`/images/intro/sangsang/review04.png`}
-            alt=""
-          />
-          <Image
-            width={450 / 2}
-            height={490 / 2}
-            src={`/images/intro/sangsang/review05.png`}
-            alt=""
-          />
-          <Image
-            width={450 / 2}
-            height={490 / 2}
-            src={`/images/intro/sangsang/review06.png`}
-            alt=""
-          />
-        </div>
-      </div>
-
-      <div className="flex justify-center items-center flex-col">
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={477}
-            height={103}
-            src={`/images/intro/sangsang/4.png`}
-            alt=""
-          />
-        </div>
-        <div className="flex justify-center items-center flex-row ">
-          <div className="h-[400px] flex justify-center items-center flex-1">
-            <Image
-              width={414}
-              height={174}
-              src={`/images/intro/sangsang/5.png`}
-              alt=""
-            />
-          </div>
-          <div className="flex justify-center items-center flex-1 flex-col">
-            <Image
-              width={630}
-              height={501}
-              src={`/images/intro/sangsang/5-1.png`}
-              alt=""
-            />
-            <Image
-              width={630}
-              height={43}
-              src={`/images/intro/sangsang/5-2.png`}
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40">
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={559}
-            height={225}
-            src={`/images/intro/sangsang/6.png`}
-            alt=""
-          />
-        </div>
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={1200}
-            height={347}
-            src={`/images/intro/sangsang/6-1.png`}
-            alt=""
-          />
-        </div>
-      </div>
-
-      <div className="flex justify-center flex-col items-center p-20 pb-40">
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={490}
-            height={103}
-            src={`/images/intro/sangsang/7.png`}
-            alt=""
-          />
-        </div>
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={1200}
-            height={354}
-            src={`/images/intro/sangsang/7-1.png`}
-            alt=""
-          />
-        </div>
-      </div>
-
-      <div className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40">
-        <div className="h-[400px] flex justify-center items-center">
-          <Image
-            width={608}
-            height={104}
-            src={`/images/intro/sangsang/8.png`}
-            alt=""
-          />
-        </div>
-        <div className="w-full flex justify-center items-center">
-          <div className="h-[400px] flex justify-center items-center flex-1">
-            <Image
-              width={636}
-              height={271}
-              src={`/images/intro/sangsang/8-1.png`}
-              alt=""
-            />
-          </div>
-          <div className="h-[400px] flex justify-center items-center flex-1">
-            <Image
-              width={600}
-              height={400}
-              src={`/images/intro/sangsang/8-4.png`}
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="w-full flex justify-center items-center">
-          <div className="flex justify-center items-center basis-1/2">
-            <Image
-              width={600}
-              height={385}
-              src={`/images/intro/sangsang/8-2.png`}
-              alt=""
-            />
-          </div>
-          <div className="flex justify-center items-center basis-1/2">
-            <Image
-              width={314}
-              height={359}
-              src={`/images/intro/sangsang/8-3.png`}
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justify-center flex-col items-center p-20 pb-40">
-        <div className="h-[200px] flex justify-center items-center">
-          <Image
-            width={481}
-            height={107}
-            src={`/images/intro/sangsang/9.png`}
-            alt=""
-          />
-        </div>
-        <div className="h-[200px] flex justify-center items-center">
-          <Image
-            width={326}
-            height={93}
-            src={`/images/intro/sangsang/9-1.png`}
-            alt=""
-          />
-        </div>
-        <div className="w-full flex justify-center items-center">
-          <div className="flex justify-center items-center basis-1/4">
-            <Image
-              width={260}
-              height={300}
-              src={`/images/intro/sangsang/9-2.jpg`}
-              alt=""
-            />
-          </div>
-          <div className="flex justify-center items-center basis-1/4">
-            <Image
-              width={260}
-              height={300}
-              src={`/images/intro/sangsang/9-3.jpg`}
-              alt=""
-            />
-          </div>
-          <div className="flex justify-center items-center basis-1/4">
-            <Image
-              width={260}
-              height={300}
-              src={`/images/intro/sangsang/9-4.jpg`}
-              alt=""
-            />
-          </div>
-          <div className="flex justify-center items-center basis-1/4">
-            <Image
-              width={260}
-              height={300}
-              src={`/images/intro/sangsang/9-5.jpg`}
-              alt=""
-            />
-          </div>
-        </div>
-        <div className="h-[200px] flex justify-center items-center">
-          <Image
-            width={1200}
-            height={130}
-            src={`/images/intro/sangsang/9-6.jpg`}
-            alt=""
-          />
-        </div>
+      <div className="flex justify-center items-center">
+        <Image
+          width={1200}
+          height={2006}
+          src={`/images/intro/howcoding/3.png`}
+          alt=""
+        />
       </div>
 
       <div
-        className="flex justify-center flex-col items-center p-20 pb-40"
+        className="flex justify-center flex-col items-center bg-gray-300 p-20 pb-40"
         style={{
-          backgroundImage: `url('/images/intro/sangsang/10-back.jpg')`,
+          backgroundImage: `url('/images/intro/howcoding/4-back.png')`,
           backgroundRepeat: `no-repeat`,
           backgroundPosition: `center`,
           backgroundSize: "100% 100%",
@@ -265,74 +50,155 @@ export default function HowcodingHome() {
       >
         <div className="h-[400px] flex justify-center items-center">
           <Image
-            width={275}
-            height={92}
-            src={`/images/intro/sangsang/10.png`}
+            width={850}
+            height={143}
+            src={`/images/intro/howcoding/4.png`}
             alt=""
           />
         </div>
         <div className="flex flex-nowrap">
           <Image
-            width={261}
-            height={415}
-            src={`/images/intro/sangsang/10-1.png`}
+            width={450 / 2}
+            height={521 / 2}
+            src={`/images/intro/howcoding/review01.png`}
             alt=""
-            className="p-1"
           />
           <Image
-            width={261}
-            height={415}
-            src={`/images/intro/sangsang/10-2.png`}
+            width={450 / 2}
+            height={521 / 2}
+            src={`/images/intro/howcoding/review02.png`}
             alt=""
-            className="p-1"
           />
           <Image
-            width={261}
-            height={415}
-            src={`/images/intro/sangsang/10-3.png`}
+            width={450 / 2}
+            height={521 / 2}
+            src={`/images/intro/howcoding/review03.png`}
             alt=""
-            className="p-1"
           />
         </div>
         <div className="flex flex-nowrap">
           <Image
-            width={261}
-            height={415}
-            src={`/images/intro/sangsang/10-4.png`}
+            width={450 / 2}
+            height={521 / 2}
+            src={`/images/intro/howcoding/review04.png`}
             alt=""
-            className="p-1"
           />
           <Image
-            width={261}
-            height={415}
-            src={`/images/intro/sangsang/10-5.png`}
+            width={450 / 2}
+            height={521 / 2}
+            src={`/images/intro/howcoding/review05.png`}
             alt=""
-            className="p-1"
           />
           <Image
-            width={261}
-            height={415}
-            src={`/images/intro/sangsang/10-6.png`}
+            width={450 / 2}
+            height={521 / 2}
+            src={`/images/intro/howcoding/review06.png`}
             alt=""
-            className="p-1"
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <Image
+          width={1200}
+          height={1320}
+          src={`/images/intro/howcoding/5.png`}
+          alt=""
+        />
+      </div>
+
+
+      <div className="flex justify-center flex-col items-center p-20 pb-40">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={743}
+            height={129}
+            src={`/images/intro/howcoding/6.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <Image
+            width={1200}
+            height={1078}
+            src={`/images/intro/howcoding/6-1.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <Image
+            width={1200}
+            height={666}
+            src={`/images/intro/howcoding/6-2.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <Image
+          width={1200}
+          height={4730}
+          src={`/images/intro/howcoding/7.png`}
+          alt=""
+        />
+      </div>
+
+      <div className="flex justify-center flex-col items-center p-20 pb-40">
+        <div className="h-[400px] flex justify-center items-center">
+          <Image
+            width={640}
+            height={208}
+            src={`/images/intro/howcoding/8.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col items-center bg-[#455080] py-20">
+        <div className="flex justify-center items-center">
+          <Image
+            width={1066}
+            height={398}
+            src={`/images/intro/howcoding/9.png`}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col items-center p-20 pb-40">
+        <div className="flex justify-center items-center">
+          <Image
+            width={1200}
+            height={588}
+            src={`/images/intro/howcoding/10.png`}
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <Image
+            width={1200}
+            height={3150}
+            src={`/images/intro/howcoding/10-1.png`}
+            alt=""
           />
         </div>
       </div>
 
       <div className="flex justify-center items-center">
         <div
-          className="flex justify-center items-center w-full"
+          className="w-full flex justify-center items-center w-full h-[480px] bg-gray-300"
           style={{
-            backgroundImage: `url('/images/intro/sangsang/12-back.webp')`,
+            backgroundImage: `url('/images/intro/howcoding/11-back.png')`,
             backgroundRepeat: `no-repeat`,
             backgroundPosition: `center`,
             backgroundSize: "100% 100%",
           }}
         >
           <Image
-            width={1200}
-            height={1218}
-            src={`/images/intro/sangsang/12.webp`}
+            width={821}
+            height={101}
+            src={`/images/intro/howcoding/11.png`}
             alt=""
           />
         </div>
