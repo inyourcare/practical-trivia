698583f2954b8270baab38f6d42e676a0bf65e37
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-04 16:32:11 +0900

    상암점

---
+++ app/intro/wawa/sangam/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "마포 상암동 학원::와와학습코칭센터 - 상암점",
+  description:
+    "개인별 맞춤의 참여형 수업을 경험 해 보세요.",
+  keywords:
+    "국어,영어,수학,과학,초등,중등,고등,개인별,맞춤,학원,와와",
+};
+
+
+export default function GoodoLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
+}
