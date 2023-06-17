3b6117b3772f2bde4a7f8e7a95252848a1fe9ae8
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-17 14:43:13 +0900

    푸터에 메일 주소 첨부

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="my-16 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-6 mx-auto  border-t-2 border-grey-100" >
-            <span className="text-center text-base"> Copyright by <a className='text-current' href=""> 실용주의 잡학사전 </a> </span>
-        </footer>
-    );
+  return (
+    <footer className="my-16 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-6 mx-auto  border-t-2 border-grey-100">
+      <span className="text-center text-base">
+        {" "}
+        {/* Copyright by{" "} */}
+        Copyright © {new Date().getFullYear()}
+        <a className="text-current" href="">
+          {" "}
+          실용주의 잡학사전.{" "}
+        </a>{" "}
+        <br/>
+        All rights reserved.
+        <br/>
+        practicaltrivia@gmail.com
+      </span>{" "}
+    </footer>
+  );
 }
 
 export default Footer;
