477e5742da9aae104a65cb7ef377cf1ffda7cb1d
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-17 16:17:44 +0900

    문의하기 개선

--- app/consultation/[slug]/page.tsx
+++ app/consultation/[slug]/page.tsx
@@ -26,6 +26,15 @@ export default function ConsultationHome({
         <div className="w-full max-w-screen-lg">
           <div className="my-12 prose prose-stone lg:prose-lg mx-auto">
             {/* {`${post.data.tags} ${(post.data.tags as Array<string>).includes("Sangsang")}`} */}
+            <h2>문의하기</h2>
+            <div className="text-sm">
+              아래 문의하기 버튼을 누르면 전문 상담사가 연락을 드립니다.
+              <br/>
+              개인정보가 부담스러운 분들은 practicaltrivia@gmail.com 로 메일 주셔도 좋습니다! :D
+            </div>
+            <div className="not-prose">
+              <hr className="mt-[2em] mb-[2em]"/>
+            </div>
             <ConsultationForm tags={post.data.tags}/>
           </div>
         </div>
