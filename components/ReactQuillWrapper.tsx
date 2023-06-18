06e89fc7a163688019582c0ed44a1a573d9a8be0
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-18 14:29:50 +0900

    미리보기 등 중간저장

--- components/ReactQuillWrapper.tsx
+++ components/ReactQuillWrapper.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
+import Markdown from "markdown-to-jsx";
 
 function ReactQuillWrapper() {
   const initialState = {
@@ -34,6 +35,7 @@ function ReactQuillWrapper() {
     ],
     title: "",
     content: "",
+    desc: "",
   };
   const [state, setState] = useState(initialState);
   const handleChange = (value: string) => {
@@ -42,6 +44,10 @@ function ReactQuillWrapper() {
   function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
     setState({ ...state, title: event.target.value });
+  }
+  function handleDescChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setState({ ...state, desc: event.target.value });
   }
   function submitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -80,18 +86,37 @@ function ReactQuillWrapper() {
               onChange={(e) => handleTitleChange(e)}
               required
             />
-            <ReactQuill
+            <label htmlFor="desc">Description</label>
+            <input
+              type="text"
+              value={state.desc}
+              name="desc"
+              placeholder="Enter a desc"
+              onChange={(e) => handleDescChange(e)}
+              required
+            />
+            {/* <ReactQuill
               value={state.content}
               modules={state.modules}
               formats={state.formats}
               onChange={handleChange}
+            /> */}
+            <textarea
+              className="w-full border h-[600px]"
+              value={state.content}
+              onChange={(e) => handleChange(e.target.value)}
             />
             <button>Save</button>
             {/* <p>{state.content}</p> */}
           </form>
         </div>
         {/* <div className="w-1/2">{state.content}</div> */}
-        <div className="w-1/2 prose prose-stone" dangerouslySetInnerHTML={{ __html: state.content }}></div>;
+        <div className="w-1/2 flex justify-center">
+          <div className=" prose prose-stone">
+            <Markdown>{state.content}</Markdown>
+          </div>
+        </div>
+        {/* <div className="w-1/2 prose prose-stone" dangerouslySetInnerHTML={{ __html: state.content }}></div> */}
       </div>
     </>
   );
