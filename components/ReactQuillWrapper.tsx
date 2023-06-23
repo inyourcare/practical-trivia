e3cb54ae4d49bd0245aa648069d249f10bc47bb7
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-23 12:41:22 +0900

    태그 추가 기능 추가

--- components/ReactQuillWrapper.tsx
+++ components/ReactQuillWrapper.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
 import Markdown from "markdown-to-jsx";
+import { tagDict } from "./util/tag/tagToKr";
 
 function ReactQuillWrapper() {
   const initialState = {
@@ -36,12 +37,19 @@ function ReactQuillWrapper() {
     title: "",
     content: "",
     desc: "",
+    id: "",
+    // tagDict: tagDict,
   };
   const [html, setHTML] = useState(true);
+  const [tagSelected] = useState<Set<string>>(new Set([]));
   const [state, setState] = useState(initialState);
   const handleChange = (value: string) => {
     setState({ ...state, content: value });
   };
+  function handleIdChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setState({ ...state, id: event.target.value });
+  }
   function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
     setState({ ...state, title: event.target.value });
@@ -73,13 +81,42 @@ function ReactQuillWrapper() {
         console.log(data);
       });
   }
+
+  const tagItemsRef = useRef<HTMLSpanElement[]>([]);
+  useEffect(() => {
+    tagItemsRef.current.map((ref) => {
+      ref.addEventListener("click", (e) => {
+        const val = ref.innerText;
+        if (tagSelected?.has(val)) {
+          ref.style.color = "black";
+          tagSelected.delete(val);
+        } else {
+          ref.style.color = "red";
+          tagSelected?.add(val);
+        }
+      });
+    });
+  }, [tagSelected]);
+
   return (
     <>
       <div className="flex flex-row">
         <div className="w-1/2">
           <form onSubmit={(e) => submitHandler}>
+            <label htmlFor="id">Id</label>
+            <input
+              className="border ml-1"
+              type="text"
+              value={state.id}
+              name="id"
+              placeholder="id -> 1"
+              onChange={(e) => handleIdChange(e)}
+              required
+            />
+            <br />
             <label htmlFor="title">Title</label>
             <input
+              className="border ml-1"
               type="text"
               value={state.title}
               name="title"
@@ -87,8 +124,10 @@ function ReactQuillWrapper() {
               onChange={(e) => handleTitleChange(e)}
               required
             />
+            <br />
             <label htmlFor="desc">Description</label>
             <input
+              className="border ml-1"
               type="text"
               value={state.desc}
               name="desc"
@@ -96,9 +135,30 @@ function ReactQuillWrapper() {
               onChange={(e) => handleDescChange(e)}
               required
             />
-
+            <br />
+            {/* {`${state.tagDict}`} */}
+            <div className="w-full">
+              tags:
+              <div className="w-full flex flex-row flex-wrap">
+                {Object.entries(tagDict).map((entry, i) => (
+                  <span
+                    key={i}
+                    ref={(elem) => {
+                      tagItemsRef.current[i] = elem as HTMLSpanElement;
+                    }}
+                    // className={`cursor-pointer ${tagSelected.has(entry[0])?'text-red':'text-black'}`}
+                    className={`cursor-pointer px-1`}
+                    // onClick={(e) => {}}
+                  >
+                    {entry[0]}
+                  </span>
+                ))}
+              </div>
+            </div>
             <nav className="gap-3 sm:gap-5 md:gap-10 lg:gap-10 xl:gap-10 2xl:gap-10 inline-flex justify-center sm:justify-center md:justify-right mt-5 sm:mt-5 md:mt-0 lg:mt-0 xl:mt-0 2xl:mt-0  items-start text-black text-left font-medium">
-              <div className="cursor-pointer" onClick={() => setHTML(!html)}>html</div>
+              <div className="cursor-pointer" onClick={() => setHTML(!html)}>
+                html
+              </div>
             </nav>
             {html === true ? (
               <>
