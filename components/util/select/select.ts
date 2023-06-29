40c540f8cf2668c328f59eb00b858b2f46a5a47b
Kwon KiHoon <inyourcareallen@gmail.com>
2023-06-30 08:57:22 +0900

    select 함수 간소화

--- components/util/select/select.ts
+++ components/util/select/select.ts
@@ -1,4 +1,4 @@
-function scrollFollowing(arr: Element[],selectingClass: string) {
+function scrollFollowing(arr: Element[], selectingClass: string) {
   const top = arr
     .filter((item) => item.classList.contains(selectingClass))
     .pop()
@@ -11,56 +11,53 @@ function scrollFollowing(arr: Element[],selectingClass: string) {
     });
   }
 }
-const select = (arr: Element[], selectingClass: string, finalCallback: any) => {
+
+function selecting(
+  arr: Element[],
+  selectingClass: string,
+  finalCallback: any,
+  intervalSeconds: number,
+  timeoutSeconds: number
+) {
   let idx = Math.floor(Math.random() * (arr.length - 0 + 1) + 0);
-  if (arr.length < 1) {
-    finalCallback();
-    return;
-  }
   const intervalId = setInterval(() => {
     const realIdx = idx % arr.length;
     const prevIdx = (arr.length + idx - 1) % arr.length;
     arr[prevIdx].classList.remove(selectingClass);
     arr[realIdx].classList.add(selectingClass);
     idx += 1;
-    scrollFollowing(arr,selectingClass)
-  }, 50);
+    scrollFollowing(arr, selectingClass);
+  }, intervalSeconds);
   const timeoutId = setTimeout(() => {
-    console.log("clear timeout");
-    clearTimeout(timeoutId);
-    clearInterval(intervalId);
+    // console.log("clear timeout3");
+    clearTimeout(intervalId);
+    clearInterval(timeoutId);
     // setState({ ...state, lsLoading: false });
-    const intervalId2 = setInterval(() => {
-      const realIdx = idx % arr.length;
-      const prevIdx = (arr.length + idx - 1) % arr.length;
-      arr[prevIdx].classList.remove(selectingClass);
-      arr[realIdx].classList.add(selectingClass);
-      idx += 1;
-      scrollFollowing(arr,selectingClass)
-    }, 100);
-    const timeoutId2 = setTimeout(() => {
-      console.log("clear timeout2");
-      clearTimeout(timeoutId2);
-      clearInterval(intervalId2);
-      // setState({ ...state, lsLoading: false });
-
-      const intervalId3 = setInterval(() => {
-        const realIdx = idx % arr.length;
-        const prevIdx = (arr.length + idx - 1) % arr.length;
-        arr[prevIdx].classList.remove(selectingClass);
-        arr[realIdx].classList.add(selectingClass);
-        idx += 1;
-        scrollFollowing(arr,selectingClass)
-      }, 200);
-      const timeoutId3 = setTimeout(() => {
-        console.log("clear timeout3");
-        clearTimeout(timeoutId3);
-        clearInterval(intervalId3);
-        // setState({ ...state, lsLoading: false });
-        finalCallback();
-      }, 1000);
-    }, 2000);
-  }, 3000);
+    finalCallback();
+  }, timeoutSeconds);
+}
+const select = (arr: Element[], selectingClass: string, finalCallback: any) => {
+  // let idx = Math.floor(Math.random() * (arr.length - 0 + 1) + 0);
+  if (arr.length < 1) {
+    finalCallback();
+    return;
+  }
+  selecting(
+    arr,
+    selectingClass,
+    () =>
+      selecting(
+        arr,
+        selectingClass,
+        () => selecting(arr, selectingClass, finalCallback, 200, 1000),
+        // finalCallback,
+        100,
+        2000
+      ),
+    // finalCallback,
+    50,
+    3000
+  );
 };
 
 export default select;
