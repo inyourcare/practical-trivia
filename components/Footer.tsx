64b952be3004a5ac648b615b8001985feebe6654
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-01 20:27:03 +0900

    폰으로 볼때 푸터쪽 에러가 있어서 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,15 @@ function Footer() {
   return (
     <footer className="my-16 flex flex-col justify-center w-6/6 sm:w-[610px] md:w-[610px] lg:w-[610px] xl:w-[610px] 2xl:w-[610px] p-6 mx-auto  border-t-2 border-grey-100">
       <span className="text-center text-base">
-        {" "}
-        {/* Copyright by{" "} */}
-        Copyright © {new Date().getFullYear()}
-        <a className="text-current" href="">
-          {" "}
-          실용주의 잡학사전.{" "}
-        </a>{" "}
-        <br/>
-        All rights reserved.
-        <br/>
-        practicaltrivia@gmail.com
-      </span>{" "}
+        <p>
+          {`Copyright © ${new Date().getFullYear()}`}
+          <a className="text-current" href="">
+            {`실용주의 잡학사전`}
+          </a>
+        </p>
+        <p>{`All rights reserved`}</p>
+        <p>{`practicaltrivia@gmail.com`}</p>
+      </span>
     </footer>
   );
 }
