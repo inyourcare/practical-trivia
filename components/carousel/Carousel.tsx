f4f459de6febdd99ad8cf37283896ff4bb0974e1
Kwon KiHoon <inyourcareallen@gmail.com>
2023-07-06 17:19:44 +0900

    이미지 슬라이더 완성

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
 import Slider from "./Slider";
+import { useRef, useEffect, useState } from "react";
 
 export default function Carousel({ children }: { children: React.ReactNode }) {
+  const [curItemIdx, setCurItemIdx] = useState(0);
   const imageSrcs = [
     "bibimbap-gb0ce9688b_1280.jpg",
     // "chicken-g95e14ed32_1280.jpg",
@@ -17,49 +20,58 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
     "meat-g15ca3b422_1280.jpg",
     "tteokbokki-g0c7fc890a_1280.jpg",
   ];
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     const sliderElem = sliderRef.current;
-  //     if (sliderElem && sliderElem.children.length > 0) {
-  //       sliderElem.insertBefore(
-  //         sliderElem.children.item(sliderElem.children.length - 1) as Element,
-  //         sliderElem.children.item(0)
-  //       );
-  //       // sliderElem.setAttribute('style','`transform: translate3d(20px,0px, 0px); transition-duration: 350ms;')
-  //     }
-  //   }, 4500);
-  //   return () => clearInterval(intervalId);
-  // }, []);
-  // const sliderRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    var idx = 1;
+    var classString = `test`;
+    // var prevClassString = "prev";
+    const intervalId = setInterval(() => {
+      const sliderElem = sliderRef.current;
+      if (sliderElem && sliderElem.children.length > 1) {
+        console.log("carousel interval", idx, classString);
+        // prevClassString = classString;
+        // sliderElem.classList.remove(classString);
+        // classString = `-translate-x-[${idx * 100}%]`;
+        idx = (idx + 1) % sliderElem.children.length;
+        // sliderElem.classList.add(classString);
+        sliderElem.setAttribute(
+          "style",
+          `transform: translate3d(-${idx * 100}%, 0px, 0px); transition-duration: 350ms;`
+        );
+      }
+    }, 4500);
+  }, []);
+  const sliderRef = useRef<HTMLDivElement>(null);
   return (
+    <div
+      className={`relative w-full h-[40vh] flex justify-center items-center flex-col overflow-hidden `}
+      // bg-[url('/images/roulette/carousel/${imageSrcs[0]}')] bg-no-repeat bg-center bg-cover`}
+    >
+      <div
+        id="carousel-slider"
+        ref={sliderRef}
+        className="w-full h-full flex flex-row transition ease-in-out delay-150 duration-300 "
+      >
+        {imageSrcs.map((src, i) => (
+          <Image
+            width={1280}
+            height={853}
+            key={i}
+            src={`/images/roulette/carousel/${src}`}
+            // fill
+            style={{ objectFit: "cover" }}
+            alt=""
+            // className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+          ></Image>
+        ))}
+      </div>
+      <div className="absolute w-full">{children}</div>
+    </div>
+
     // <div
     //   className={`relative w-full h-[40vh] flex justify-center items-center flex-col overflow-hidden`}
-    //   // bg-[url('/images/roulette/carousel/${imageSrcs[0]}')] bg-no-repeat bg-center bg-cover`}
     // >
-    //   <div
-    //     id="carousel-slider"
-    //     ref={sliderRef}
-    //     className="w-full h-full flex flex-row "
-    //   >
-    //     {imageSrcs.map((src, i) => (
-    //       <Image
-    //         width={1280}
-    //         height={853}
-    //         key={i}
-    //         src={`/images/roulette/carousel/${src}`}
-    //         // fill
-    //         style={{ objectFit: "cover" }}
-    //         alt=""
-    //       ></Image>
-    //     ))}
-    //   </div>
-    //   <div className="absolute w-full">{children}</div>
-    // </div>
-    // <div
-    //   className={`relative w-full h-[40vh] flex justify-center items-center flex-col overflow-hidden`}
-    // >
-      <Slider items={imageSrcs}>{children}</Slider>
-      // {/* <div className="absolute w-full">{children}</div> */}
+    // <Slider items={imageSrcs}>{children}</Slider>
+    // {/* <div className="absolute w-full">{children}</div> */}
     // </div>
   );
 }
